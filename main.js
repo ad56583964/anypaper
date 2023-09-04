bab5c9cd297906888867fa0578d24cd76aa63f0c
emasi <1176949486@qq.com>
2023-09-05 00:46:35 +0800

    add konva

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// 导入 Konva
+import Konva from 'konva';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// 获取窗口宽高
+const width = window.innerWidth;
+const height = window.innerHeight - 25;
 
-setupCounter(document.querySelector('#counter'))
+// 创建 Konva 舞台和图层
+const stage = new Konva.Stage({
+  container: 'container',
+  width: width,
+  height: height,
+});
+
+const layer = new Konva.Layer();
+stage.add(layer);
+
+let isPaint = false;
+let mode = 'brush';
+let lastLine;
+
+stage.on('mousedown touchstart', (e) => {
+  isPaint = true;
+  const pos = stage.getPointerPosition();
+  lastLine = new Konva.Line({
+    stroke: '#df4b26',
+    strokeWidth: 5,
+    globalCompositeOperation:
+      mode === 'brush' ? 'source-over' : 'destination-out',
+    lineCap: 'round',
+    lineJoin: 'round',
+    points: [pos.x, pos.y, pos.x, pos.y],
+  });
+  layer.add(lastLine);
+});
+
+stage.on('mouseup touchend', () => {
+  isPaint = false;
+});
+
+stage.on('mousemove touchmove', (e) => {
+  if (!isPaint) {
+    return;
+  }
+
+  e.evt.preventDefault();
+  const pos = stage.getPointerPosition();
+  const newPoints = lastLine.points().concat([pos.x, pos.y]);
+  lastLine.points(newPoints);
+});
+
+const select = document.getElementById('tool');
+select.addEventListener('change', () => {
+  mode = select.value;
+});
