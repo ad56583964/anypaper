e6e221924a635b5493b449a3b75564ab9f7bf4c5
emasi <ad56583964@outlook.com>
2025-01-06 00:05:42 +0800

    standalone select logic

--- select.js
+++ select.js
@@ -2,6 +2,26 @@ let DEBUG_INFO = console.log;
 
 export default class SelectTool {
     constructor(table){
+        this.table = table;
+        
+        this.currentBlock = {
+            x: 0,
+            y: 0
+        }
+        // visual attrs over table
+        this.hangingBlock = new Konva.Rect({
+            width: 20,
+            height: 40,
+            fill: "green",
+        })
+        this.selectedBlock = new Konva.Rect({
+            width: 20,
+            height: 40,
+            fill: "orange",
+        })
+
+        this.table.gLayer.add(this.hangingBlock)
+
         DEBUG_INFO("Init PencilTool");
     }
 
@@ -13,7 +33,98 @@ export default class SelectTool {
         DEBUG_INFO("PencilTool deactivate");
     }
 
+    /* realize functions */
+    selectBlock(){
+        if(!this._isOutside()){
+            this.selectedBlock.setAttrs({
+                x: this.currentBlock.x * this.table.block.width,
+                y: this.currentBlock.y * this.table.block.height,
+            })
+            this.table.gLayer.add(this.selectedBlock)
+        }
+    }
+    
+    getcurrentBlock(){
+        return {
+            x:Math.floor((this.table.currentPointer.x) / this.table.block.width),
+            y:Math.floor((this.table.currentPointer.y) / this.table.block.height)
+        }
+    }
+
+    _updateHangingBlock(x,y){
+        this.hangingBlock.setAttrs({
+            x: x * this.table.block.width,
+            y: y * this.table.block.height,
+            visible: true
+        })
+    }
+
+    _hideHangingBlock(){
+        this.hangingBlock.setAttrs({
+            visible: false
+        })
+    }
+
+    _isOutside(){
+        DEBUG_INFO("height"+this.table.konva_attr.height()+" width"+this.table.konva_attr.width());
+        // seems stage will auto extern
+        DEBUG_INFO("height"+this.table.stage.height()+" width"+this.table.stage.width());
+        return this.table.currentPointer.x*this.table.zoom.current < 0 || this.table.currentPointer.x > this.table.konva_attr.width() || this.table.currentPointer.y < 0 || this.table.currentPointer.y > this.table.konva_attr.height()
+    }
+
+    move(e){
+        // the only e.client entrypoint
+        this.gPointer = this.table.stage.getPointerPosition();
+        
+        var gLayerPos = this.table.gLayer.getAbsolutePosition()
+
+        this.table.currentPointer = {
+            x:(this.gPointer.x - gLayerPos.x) / this.table.zoom.current,
+            y:(this.gPointer.y - gLayerPos.y) / this.table.zoom.current,
+        }
+
+        if (window.DebugBarComponentRef && window.DebugBarComponentRef.current) {
+            // console.log("hold Ref")
+            window.DebugBarComponentRef.current.updateMousePosition(this.table.currentPointer.x,this.table.currentPointer.y)
+        }
+        else{
+            console.log("no Ref")
+        }
+
+        this.currentBlock = this.getcurrentBlock()
+        // 此处的 currentBlock 实际是指 gLayer 为坐标系起点
+        // log is effect the performance
+        // DEBUG_INFO("currentPointer:",this.table.currentPointer.x - gLayerPos.x , this.table.currentPointer.y - gLayerPos.y);
+        DEBUG_INFO("currentBlock:",this.getcurrentBlock());
+
+        if(this._isOutside()){
+            this._hideHangingBlock()
+        }
+        else{
+            this._updateHangingBlock(this.currentBlock.x,this.currentBlock.y)
+        }
+
+        DEBUG_INFO(this._isOutside())
+    }
+
+    // Event listeners
     pointermove(e){
         DEBUG_INFO("PencilTool pointermove");
+        this.move(e);
     }
+
+    pointerdown(e){
+        DEBUG_INFO("PencilTool pointerdown");
+        this.selectBlock();
+    }
+
+    wheel(e){
+        DEBUG_INFO("PencilTool wheel");
+
+        const delta = e.deltaY;
+        DEBUG_INFO("delta: ",delta);
+
+        this.table.updateZoom(e);
+    }
+
 }
--- table.js
+++ table.js
@@ -2,6 +2,7 @@ import Konva from "konva";
 import AnyA4Tool from "./commands";
 import PaperTool from "./paper";
 import PencilTool from "./pencil";
+import SelectTool from "./select";
 
 let DEBUG_INFO = console.log;
 
@@ -9,11 +10,6 @@ export default class Table {
     constructor(containerId = 'a4-table', theme) {
         this.width = 20*40;
         this.height = 20*30;
-
-        this.currentBlock = {
-            x: 0,
-            y: 0
-        }
 
         this.zoom = {
             current: 1,
@@ -47,20 +43,6 @@ export default class Table {
 
         this.stage.container().style.cursor = 'crosshair'
 
-        this.hangingBlock = new Konva.Rect({
-            width: 20,
-            height: 40,
-            fill: "green",
-        })
-
-        this.selectedBlock = new Konva.Rect({
-            width: 20,
-            height: 40,
-            fill: "orange",
-        })
-
-        this.gLayer.add(this.hangingBlock)
-
         // Draw table
         this.initTable();
 
@@ -68,13 +50,15 @@ export default class Table {
         this.currentTool = null;
         this.tools = {};
 
+        // this.action = new TableAction(this)
+
         this.registerTool("select",new SelectTool(this))
-        this.registerTool("paper",new PaperTool(this))
+        // this.registerTool("paper",new PaperTool(this))
         this.registerTool("pencil",new PencilTool(this))
 
-        this.action = new TableAction(this)
-        this.debug = new TableDebug(this)
-        this.eventManage = new TableEvent(this)
+        this.setActiveTool("select")
+        // this.debug = new TableDebug(this)
+        // this.eventManage = new TableEvent(this)
     }
 
     /**
@@ -111,16 +95,16 @@ export default class Table {
      * init debug hit
      */
     initTable() {
-        this.table = new Konva.Rect({
+        this.konva_attr = new Konva.Rect({
             width: this.stage.width(),
             height: this.stage.height(),
             //grey
             fill: "grey",
         });
 
-        this.gLayer.add(this.table);
+        this.gLayer.add(this.konva_attr);
         // 确保背景在其他元素的下方(konva api)
-        this.table.moveToBottom();
+        this.konva_attr.moveToBottom();
 
 
         this.initGridBG();
@@ -138,42 +122,42 @@ export default class Table {
         });
     }
 
-    selectBlock(x,y){
-        if(!this._isOutside()){
-            this.selectedBlock.setAttrs({
-                x: this.currentBlock.x * this.block.width,
-                y: this.currentBlock.y * this.block.height,
-            })
-            this.gLayer.add(this.selectedBlock)
-        }
-    }
-    
-    getcurrentBlock(){
-        return {
-            x:Math.floor((this.currentPointer.x) / this.block.width),
-            y:Math.floor((this.currentPointer.y) / this.block.height)
-        }
-    }
+    updateZoom(e){
+        const scroll = e.deltaY;
+        var oldScale = this.zoom.current
 
-    _updateHangingBlock(x,y){
-        this.hangingBlock.setAttrs({
-            x: x * this.block.width,
-            y: y * this.block.height,
-            visible: true
+        // determine the zoom direction
+        if(scroll < 0)        
+            this.zoom.current += 0.1
+        else if(scroll > 0)
+            this.zoom.current -= 0.1
+
+        // limit the zoom range
+        if(this.zoom.current < this.zoom.min)
+            this.zoom.current = this.zoom.min
+        if(this.zoom.current > this.zoom.max)
+            this.zoom.current = this.zoom.max
+        
+        // step1: scale the glayer
+        this.gLayer.scaleX(this.zoom.current)
+        this.gLayer.scaleY(this.zoom.current)
+
+        // step2: move the glayer
+        // move to the pointer as center
+        const pointer = this.stage.getPointerPosition();
+
+        const mousePointTarget = {
+            x: (pointer.x - this.gLayer.x()) / oldScale,
+            y: (pointer.y - this.gLayer.y()) / oldScale,
+            };
+
+        // DEBUG_INFO("gLayerXY",_px,_py);
+        this.gLayer.setAttrs({
+            x: pointer.x - mousePointTarget.x * this.zoom.current,
+            y: pointer.y - mousePointTarget.y * this.zoom.current,
         })
-    }
 
-    _hideHangingBlock(){
-        this.hangingBlock.setAttrs({
-            visible: false
-        })
-    }
-
-    _isOutside(){
-        DEBUG_INFO("height"+this.table.height()+" width"+this.table.width());
-        // seems stage will auto extern
-        DEBUG_INFO("height"+this.stage.height()+" width"+this.stage.width());
-        return this.currentPointer.x*this.zoom.current < 0 || this.currentPointer.x > this.table.width() || this.currentPointer.y < 0 || this.currentPointer.y > this.table.height()
+        this.fitWindow();
     }
 
     /*
@@ -195,13 +179,13 @@ export default class Table {
     }
 
     setupEventListeners() {
-        const events = ['pointermove', 'pointerdown', 'pointerup', 'keydown', 'keyup'];
+        const events = ['wheel','pointermove', 'pointerdown', 'pointerup', 'keydown', 'keyup'];
         events.forEach(eventType => {
             window.addEventListener(eventType, (e) => this.handleEvent(eventType, e));
         });
 
         // 保留原有的事件
-        window.addEventListener("wheel", (e) => this.table.handleWheel(e));
+        // window.addEventListener("wheel", (e) => this.table.handleWheel(e));
     }
 
     handleEvent(eventType, event) {
@@ -209,90 +193,6 @@ export default class Table {
             this.currentTool[eventType](event);
         }
     }
-
-    move(e){
-        // the only e.client entrypoint
-        this.gPointer = this.stage.getPointerPosition();
-        
-        var gLayerPos = this.gLayer.getAbsolutePosition()
-
-        this.currentPointer = {
-            x:(this.gPointer.x - gLayerPos.x) / this.zoom.current,
-            y:(this.gPointer.y - gLayerPos.y) / this.zoom.current,
-        }
-
-        if (window.DebugBarComponentRef && window.DebugBarComponentRef.current) {
-            // console.log("hold Ref")
-            window.DebugBarComponentRef.current.updateMousePosition(this.currentPointer.x,this.currentPointer.y)
-        }
-        else{
-            console.log("no Ref")
-        }
-
-        this.currentBlock = this.getcurrentBlock()
-        // 此处的 currentBlock 实际是指 gLayer 为坐标系起点
-        // log is effect the performance
-        // DEBUG_INFO("currentPointer:",this.currentPointer.x - gLayerPos.x , this.currentPointer.y - gLayerPos.y);
-        // DEBUG_INFO("currentBlock:",this.getcurrentBlock());
-
-        if(this._isOutside()){
-            this._hideHangingBlock()
-        }
-        else{
-            this._updateHangingBlock(this.currentBlock.x,this.currentBlock.y)
-        }
-
-        DEBUG_INFO(this._isOutside())
-    }
-}
-
-class TableAction {
-    constructor(table){
-        this.table = table;
-    }
-
-    /**
-     * @param {number} scroll
-     * 
-     */
-    zoom(scroll){
-        
-        var oldScale = this.table.zoom.current
-
-        // determine the zoom direction
-        if(scroll < 0)        
-            this.table.zoom.current += 0.1
-        else if(scroll > 0)
-            this.table.zoom.current -= 0.1
-
-        // limit the zoom range
-        if(this.table.zoom.current < this.table.zoom.min)
-            this.table.zoom.current = this.table.zoom.min
-        if(this.table.zoom.current > this.table.zoom.max)
-            this.table.zoom.current = this.table.zoom.max
-        
-        // step1: scale the glayer
-        this.table.gLayer.scaleX(this.table.zoom.current)
-        this.table.gLayer.scaleY(this.table.zoom.current)
-
-        // step2: move the glayer
-        // move to the pointer as center
-        const pointer = this.table.stage.getPointerPosition();
-
-        const mousePointTarget = {
-            x: (pointer.x - this.table.gLayer.x()) / oldScale,
-            y: (pointer.y - this.table.gLayer.y()) / oldScale,
-            };
-
-        // DEBUG_INFO("gLayerXY",_px,_py);
-        this.table.gLayer.setAttrs({
-            x: pointer.x - mousePointTarget.x * this.table.zoom.current,
-            y: pointer.y - mousePointTarget.y * this.table.zoom.current,
-        })
-
-        this.table.fitWindow();
-    }
-
 }
 
 class TableDebug {
@@ -323,50 +223,50 @@ class TableDebug {
     }
 }
 
-class TableEvent {
-    constructor(table){
-        this.table = table;
+// class TableEvent {
+//     constructor(table){
+//         this.table = table;
 
-        window.addEventListener("resize", (e) => this.handleResize(e));
-        window.addEventListener("wheel", (e) => this.handleWheel(e));
-        window.addEventListener("pointermove", (e) => this.handleMove(e));
-        window.addEventListener("pointerdown", (e) => this.handleClick(e));
-        window.addEventListener("keydown", (e) => this.handleKey(e));
-    }
+//         window.addEventListener("resize", (e) => this.handleResize(e));
+//         window.addEventListener("wheel", (e) => this.handleWheel(e));
+//         window.addEventListener("pointermove", (e) => this.handleMove(e));
+//         window.addEventListener("pointerdown", (e) => this.handleClick(e));
+//         window.addEventListener("keydown", (e) => this.handleKey(e));
+//     }
 
-    handleWheel(e) {
-        DEBUG_INFO("Enter handleWheel");
+//     handleWheel(e) {
+//         DEBUG_INFO("Enter handleWheel");
 
-        this.table.debug.updateHit();
-        const delta = e.deltaY;
-        DEBUG_INFO("delta: ",delta);
+//         this.table.debug.updateHit();
+//         const delta = e.deltaY;
+//         DEBUG_INFO("delta: ",delta);
 
-        this.table.action.zoom(delta);
-    }
+//         this.table.action.zoom(delta);
+//     }
 
-    handleMove(e) {
-        // DEBUG_INFO("Enter handleMove");
-        this.table.move(e);
-        this.table.debug.updateHit();
-    }
+//     handleMove(e) {
+//         // DEBUG_INFO("Enter handleMove");
+//         this.table.move(e);
+//         this.table.debug.updateHit();
+//     }
 
-    handleClick(e){
-        DEBUG_INFO("Enter handleClick");
+//     handleClick(e){
+//         DEBUG_INFO("Enter handleClick");
         
-        if(this.table.state == "idle" || "typing"){
-            DEBUG_INFO("Enter table idle");
-            this.table.state = 'typing'
-            this.table.selectBlock();
-        }
-    }
+//         if(this.table.state == "idle" || "typing"){
+//             DEBUG_INFO("Enter table idle");
+//             this.table.state = 'typing'
+//             this.table.selectBlock();
+//         }
+//     }
 
-    handleKey(e){
-        DEBUG_INFO("Enter handleKey");
+//     handleKey(e){
+//         DEBUG_INFO("Enter handleKey");
 
-        if (event.altKey && event.code === 'Digit1') {
-            event.preventDefault(); // 阻止默认行为
-            DEBUG_INFO("Alt + 1")
-            this.table.setActiveTool("pencil");
-        }
-    }
-}
+//         if (event.altKey && event.code === 'Digit1') {
+//             event.preventDefault(); // 阻止默认行为
+//             DEBUG_INFO("Alt + 1")
+//             this.table.setActiveTool("pencil");
+//         }
+//     }
+// }
